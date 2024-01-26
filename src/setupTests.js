5c7982e38a68fa658c36bb92dfbfa0938e74354f
Ruben Gagnon <delphingagnon2@gmail.com>
2024-01-26 03:35:38 +0100

    Ajout fichiers pour les tests et index.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,17 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+<header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>
+    Edit <code>src/App.js</code> and save to reload.
+  </p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header>
