bc20971336305929e63379b969dadb865bdb5fb7
Ruben Gagnon <delphingagnon2@gmail.com>
2024-01-26 03:26:44 +0100

    Ajout du fichier data

---
+++ src/datas/catList.js
@@ -0,0 +1,65 @@
+import Cat1 from '../assets/tumblr01.jpg'
+import Cat2 from '../assets/tumblr02.jpg'
+import Cat3 from '../assets/tumblr03.jpg'
+import Cat4 from '../assets/tumblr04.jpg'
+import Cat5 from '../assets/tumblr05.jpg'
+import Cat6 from '../assets/tumblr06.jpg'
+import Cat7 from '../assets/tumblr07.jpg'
+import Cat8 from '../assets/tumblr08.jpg'
+import Cat9 from '../assets/tumblr09.jpg'
+import Cat10 from '../assets/tumblr10.jpg'
+
+
+export const catList = [
+	{
+        id: "MTgwODA3MA",
+        name: "Cat1",
+        cover: Cat1
+    },
+    {   
+        id: "tt",
+        name: "Cat2",
+        cover:Cat2
+    },
+    {   
+        id: "bmp",
+        name: "Cat3",
+        cover:Cat3
+    },
+    {   
+        id: "c8a",
+        name: "Cat4",
+        cover:Cat4
+    },
+    {   
+        id: "3kj",
+        name: "Cat5",
+        cover:Cat5
+    },
+    {   
+        id: "9pu",
+        name: "Cat6",
+        cover:Cat6
+    },
+    {   
+        id: "aca",
+        name: "Cat7",
+        cover:Cat7
+    },
+    {
+        id: "ebk",
+        name: "Cat8",
+        cover:Cat8
+    },
+    {   
+        id: "MTg0MTYyOQ",
+        name: "Cat9",
+        cover:Cat9
+    },
+    {
+        id: "6mo",
+        name: "Cat10",
+        cover:Cat10
+    }
+
+]
